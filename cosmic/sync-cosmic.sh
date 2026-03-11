5bfdbf5cf059bb1f5f76b748c7683578b5821818
Giovanni <smith.alvarez.g@gmail.com>
2026-03-11 16:41:32 -0400

    Stop tracking COSMIC configuration files

--- cosmic/sync-cosmic.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env fish
-
-# EXPORT: Save current COSMIC registry to a trackable file
-dconf dump /org/cosmic/ >~/.config/cosmic/cosmic_settings.dconf
-
-# IMPORT: Load settings from the file into the system
-# dconf load /org/cosmic/ < ~/dotfiles/cosmic/cosmic_settings.dconf
