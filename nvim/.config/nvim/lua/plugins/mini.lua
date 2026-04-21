43fefd9953ab16076dc744b5ed70668bc578651f
gsmitha <smith.alvarez.g@gmail.com>
2026-04-20 20:32:46 -0400

    feat(mini): Loading what mini.nvim modules I want

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -10,99 +10,126 @@ local M = {}
 local misc = Config.safe_require("mini.misc")
 
 local function pacer_logic(mode, f)
-  if package.loaded["mini.misc"] then
-    misc.safely(mode, f)
-  else
-    f()
-  end
+	if package.loaded["mini.misc"] then
+		misc.safely(mode, f)
+	else
+		f()
+	end
 end
 
 M.now = function(f)
-  pacer_logic("now", f)
+	pacer_logic("now", f)
 end
 M.later = function(f)
-  pacer_logic("later", f)
+	pacer_logic("later", f)
 end
 M.now_if_args = vim.fn.argc(-1) > 0 and M.now or M.later
 M.on_event = function(ev, f)
-  pacer_logic("event:" .. ev, f)
+	pacer_logic("event:" .. ev, f)
 end
 M.on_filetype = function(ft, f)
-  pacer_logic("filetype:" .. ft, f)
+	pacer_logic("filetype:" .. ft, f)
 end
 
 if package.loaded["mini.misc"] then
-  misc.setup_auto_root()
+	misc.setup_auto_root()
 end
 
 -- [ 2. IMMEDIATE SETUP (M.now) ]
 -- Critical UI components and theme that should be loaded during startup.
 M.now(function()
-  -- A. COLORSCHEME (mini.base16)
-  Config.safe_require("mini.base16").setup {
-    palette = {
-      base00 = "#1e1e2e", -- mantle
-      base01 = "#181825", -- crust
-      base02 = "#313244", -- surface0
-      base03 = "#45475a", -- surface1
-      base04 = "#585b70", -- surface2
-      base05 = "#cdd6f4", -- text
-      base06 = "#f5e0dc", -- rosewater
-      base07 = "#b4befe", -- lavender
-      base08 = "#f38ba8", -- red
-      base09 = "#fab387", -- peach
-      base0A = "#f9e2af", -- yellow
-      base0B = "#a6e3a1", -- green
-      base0C = "#94e2d5", -- teal
-      base0D = "#89b4fa", -- blue
-      base0E = "#cba6f7", -- mauve
-      base0F = "#f2cdcd", -- flamingo
-    },
-    use_cterm = nil,
-    plugins = { default = true },
-  }
+	-- A. COLORSCHEME (mini.base16)
+	Config.safe_require("mini.base16").setup({
+		palette = {
+			base00 = "#1e1e2e", -- mantle
+			base01 = "#181825", -- crust
+			base02 = "#313244", -- surface0
+			base03 = "#45475a", -- surface1
+			base04 = "#585b70", -- surface2
+			base05 = "#cdd6f4", -- text
+			base06 = "#f5e0dc", -- rosewater
+			base07 = "#b4befe", -- lavender
+			base08 = "#f38ba8", -- red
+			base09 = "#fab387", -- peach
+			base0A = "#f9e2af", -- yellow
+			base0B = "#a6e3a1", -- green
+			base0C = "#94e2d5", -- teal
+			base0D = "#89b4fa", -- blue
+			base0E = "#cba6f7", -- mauve
+			base0F = "#f2cdcd", -- flamingo
+		},
+		use_cterm = nil,
+		plugins = { default = true },
+	})
 
-  -- B. ICONS (mini.icons)
-  local icons = Config.safe_require "mini.icons"
-  icons.setup()
-  icons.mock_nvim_web_devicons()
+	-- B. ICONS (mini.icons)
+	local icons = Config.safe_require("mini.icons")
+	icons.setup()
+	icons.mock_nvim_web_devicons()
 
-  -- C. UI COMPONENTS
-  Config.safe_require("mini.tabline").setup()
-  Config.safe_require("mini.statusline").setup()
+	-- C. UI COMPONENTS
+	Config.safe_require("mini.tabline").setup()
+	Config.safe_require("mini.statusline").setup()
 end)
 
 -- [ 3. DEFERRED SETUP (M.later) ]
 -- Non-critical tools and editing enhancements loaded after startup.
 M.later(function()
-  -- A. NAVIGATION & EDITING
-  Config.safe_require("mini.files").setup()
-  Config.safe_require("mini.jump2d").setup()
-  Config.safe_require("mini.jump").setup()
-  Config.safe_require("mini.splitjoin").setup()
+	-- A. NAVIGATION & EDITING
+	Config.safe_require("mini.files").setup()
+	Config.safe_require("mini.jump2d").setup()
+	Config.safe_require("mini.jump").setup()
+	Config.safe_require("mini.splitjoin").setup()
+	Config.safe_require("mini.comment").setup()
+	Config.safe_require("mini.operators").setup()
+	Config.safe_require("mini.pairs").setup()
+	Config.safe_require("mini.surround").setup({
+		highlight_duration = 2000,
+	})
+	Config.safe_require("mini.diff").setup()
+	Config.safe_require("mini.move").setup()
+	Config.safe_require("mini.bracketed").setup()
+	Config.safe_require("mini.extra").setup()
+	Config.safe_require("mini.sessions").setup({
+		autoread = true,
+		autowrite = true,
+		directory = vim.fn.stdpath("data") .. "/sessions",
+	})
 
-  -- B. ENHANCED TEXT OBJECTS (mini.ai)
-  local ai = Config.safe_require "mini.ai"
-  ai.setup {
-    n_lines = 500,
-    custom_textobjects = {
-      f = ai.gen_spec.treesitter { a = "@function.outer", i = "@function.inner" },
-      c = ai.gen_spec.treesitter { a = "@class.outer", i = "@class.inner" },
-      o = ai.gen_spec.treesitter {
-        a = { "@conditional.outer", "@loop.outer" },
-        i = { "@conditional.inner", "@loop.inner" },
-      },
-      g = function()
-        local from = { line = 1, col = 1 }
-        local to = {
-          line = vim.fn.line "$",
-          col = math.max(vim.fn.getline("$"):len(), 1),
-        }
-        return { from = from, to = to }
-      end,
-      d = { "%d+" },
-    },
-  }
+	-- C. HIGHLIGHTING (mini.hipatterns)
+	local hipatterns = Config.safe_require("mini.hipatterns")
+	local hi_extra = require("mini.extra").gen_highlighter
+	hipatterns.setup({
+		highlighters = {
+			-- Highlight standalone 'FIXME', 'TODO', 'NOTE'
+			fixme = hi_extra.words({ "FIXME", "WIP" }, "MiniHypatternsFixme"),
+			todo = hi_extra.words({ "TODO" }, "MiniHypatternsTodo"),
+			note = hi_extra.words({ "NOTE", "INFO" }, "MiniHypatternsNote"),
+
+			-- Highlight hex colors
+			hex_color = hipatterns.gen_highlighter.hex_color(),
+		},
+	})
+
+	-- B. ENHANCED TEXT OBJECTS (mini.ai)
+	local ai = Config.safe_require("mini.ai")
+	local ai_extra = require("mini.extra").gen_ai_spec
+	ai.setup({
+		n_lines = 500,
+		custom_textobjects = {
+			f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }),
+			c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }),
+			o = ai.gen_spec.treesitter({
+				a = { "@conditional.outer", "@loop.outer" },
+				i = { "@conditional.inner", "@loop.inner" },
+			}),
+			-- From mini.extra
+			i = ai_extra.indent(),
+			b = ai_extra.buffer(),
+			d = ai_extra.diagnostic(),
+			n = ai_extra.number(),
+		},
+	})
 end)
 
 return M
--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -7,55 +7,109 @@ local M = {}
 
 local mini = Config.safe_require "plugins.mini"
 
+local wk = require("which-key")
+
 mini.later(function()
-  require("which-key").setup {
+  wk.setup {
     preset = "helix",
-    spec = {
-      -- Top level groups
-      { "<leader>c", group = "Code", icon = "" },
-      { "<leader>e", group = "Explore", icon = "󰙅" },
-      { "<leader>f", group = "Find", icon = "" },
-      { "<leader>g", group = "Git", icon = "󰊢" },
-      { "<leader>gh", group = "Git Hunk", icon = "󱖋" },
-      { "<leader>q", group = "Quit/Session", icon = "󰗼" },
-      { "<leader>s", group = "Search", icon = "󰍉" },
-      { "<leader>u", group = "UI", icon = "󰙵" },
-      { "<leader>x", group = "Execute", icon = "" },
+  }
+  wk.add {
+    -- Top level groups
+    { "<leader>c", group = "Code", icon = "" },
+    { "<leader>e", group = "Explore", icon = "󰙅" },
+    { "<leader>f", group = "Find", icon = "" },
+    { "<leader>g", group = "Git", icon = "󰊢" },
+    { "<leader>gh", group = "Git Hunk", icon = "󱖋" },
+    { "<leader>q", group = "Quit/Session", icon = "󰗼" },
+    { "<leader>s", group = "Search", icon = "󰍉" },
+    { "<leader>u", group = "UI", icon = "󰙵" },
+    { "<leader>x", group = "Execute", icon = "" },
 
-      -- Expanders for built-in info
-      {
-        "<leader>b", group = "buffer", icon = "",
-        expand = function()
-          return require("which-key.extras").expand.buf()
-        end,
-      },
-      {
-        "<leader>w", group = "window", icon = "",
-        expand = function()
-          return require("which-key.extras").expand.win()
-        end,
-      },
+    { "s", group = "surround", icon = "󰗈" },
+    { "sa", desc = "Add" },
+    { "sd", desc = "Delete" },
+    { "sr", desc = "Replace" },
+    { "sf", desc = "Find right" },
+    { "sF", desc = "Find left" },
+    { "sh", desc = "Highlight" },
+    { "sn", desc = "Update next" },
+    { "sl", desc = "Update last" },
 
-      -- Single Key Overrides
-      { "<leader><space>", icon = "󰈞" },
-      { "<leader>/", icon = "󰱽" },
+    -- Bracket Navigation (mini.bracketed)
+    { "[", group = "prev", icon = "󰮳" },
+    { "]", group = "next", icon = "󰮳" },
+    { "[b", desc = "Buffer" },
+    { "]b", desc = "Buffer" },
+    { "[d", desc = "Diagnostic" },
+    { "]d", desc = "Diagnostic" },
+    { "[f", desc = "File" },
+    { "]f", desc = "File" },
+    { "[q", desc = "Quickfix" },
+    { "]q", desc = "Quickfix" },
 
-      -- Global/Local help
-      {
-        "<leader>?",
-        function()
-          require("which-key").show { global = false }
-        end,
-        desc = "Buffer Local Keymaps",
-        icon = "󰈔",
-      },
+    -- Expanders for built-in info
+    {
+      "<leader>b",
+      group = "buffer",
+      icon = "",
+      expand = function()
+        return require("which-key.extras").expand.buf()
+      end,
+    },
+    {
+      "<leader>w",
+      group = "window",
+      icon = "",
+      expand = function()
+        return require("which-key.extras").expand.win()
+      end,
+    },
 
-      -- Text Objects (mini.ai support)
-      {
-        mode = { "o", "x" },
-        { "a", group = "around" },
-        { "i", group = "inside" },
-      },
+    -- Single Key Overrides
+    { "<leader><space>", icon = "󰈞" },
+    { "<leader>/", icon = "󰱽" },
+
+    -- Global/Local help
+    {
+      "<leader>?",
+      function()
+        wk.show { global = false }
+      end,
+      desc = "Buffer Local Keymaps",
+      icon = "󰈔",
+    },
+
+    -- Text Objects (mini.ai support)
+    {
+      mode = { "o", "x" },
+      { "a", group = "around" },
+      { "i", group = "inside" },
+      { "g", group = "goto" },
+      { "gg", desc = "first line" },
+      { "ge", desc = "prev word end" },
+      { "gE", desc = "prev WORD end" },
+      { "g_", desc = "last char" },
+      { "g,", desc = "next change" },
+      { "g;", desc = "prev change" },
+      { "s", group = "surround" },
+      { "as", desc = "around surround" },
+      { "is", desc = "inside surround" },
+      { "[", group = "prev" },
+      { "[b", desc = "buffer" },
+      { "[d", desc = "diagnostic" },
+      { "[q", desc = "quickfix" },
+      { "]", group = "next" },
+      { "]b", desc = "buffer" },
+      { "]d", desc = "diagnostic" },
+      { "]q", desc = "quickfix" },
+      { "at", desc = "tag" },
+      { "it", desc = "tag" },
+      { "af", desc = "function" },
+      { "if", desc = "function" },
+      { "ao", desc = "block" },
+      { "io", desc = "block" },
+      { "aa", desc = "argument" },
+      { "ia", desc = "argument" },
     },
   }
 end)
