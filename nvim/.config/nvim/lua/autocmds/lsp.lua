f0f549abbe0742f7f8da2002bcfaa0f58344bf3f
gsmitha <smith.alvarez.g@gmail.com>
2026-04-24 01:33:35 -0400

    snacks lsp progress bar

--- nvim/.config/nvim/lua/autocmds/lsp.lua
+++ nvim/.config/nvim/lua/autocmds/lsp.lua
@@ -66,10 +66,21 @@ local lsp_attach = function(args)
 	end
 
 	-- [[ LSP Loading Status]]
+	---@type table<number, {token:lsp.ProgressToken, msg:string, done:boolean}[]>
+	local progress = vim.defaulttable()
+
 	vim.ui.progress_status()
+
 	vim.api.nvim_create_autocmd("LspProgress", {
+		---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
 		callback = function(ev)
-			local value = ev.data.params.value
+			local client = vim.lsp.get_client_by_id(ev.data.client_id)
+			local value = ev.data.params.value --[[@as {percentage?: number, title?: string, message?: string, kind: "begin" | "report" | "end"}]]
+			if not client or type(value) ~= "table" then
+				return
+			end
+
+			-- Feed vim.ui.progress_status()
 			vim.api.nvim_echo({ { value.message or "" } }, false, {
 				id = "lsp." .. ev.data.client_id,
 				kind = "progress",
@@ -78,17 +89,45 @@ local lsp_attach = function(args)
 				status = value.kind ~= "end" and "running" or "success",
 				percent = value.percentage,
 			})
+
+			-- Feed snacks.nvim notifier
+			local p = progress[client.id]
+			for i = 1, #p + 1 do
+				if i == #p + 1 or p[i].token == ev.data.params.token then
+					p[i] = {
+						token = ev.data.params.token,
+						msg = ("[%3d%%] %s%s"):format(
+							value.kind == "end" and 100 or value.percentage or 100,
+							value.title or "",
+							value.message and (" **%s**"):format(value.message) or ""
+						),
+						done = value.kind == "end",
+					}
+					break
+				end
+			end
+			local msg = {} ---@type string[]
+			progress[client.id] = vim.tbl_filter(function(v)
+				return table.insert(msg, v.msg) or not v.done
+			end, p)
+			local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+			vim.notify(table.concat(msg, "\n"), "info", {
+				id = "lsp_progress",
+				title = client.name,
+				opts = function(notif)
+					notif.icon = #progress[client.id] == 0 and " "
+						or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+				end,
+			})
 		end,
 	})
 
-	-- Redraw statusline whenever progress state changes
 	vim.api.nvim_create_autocmd("Progress", {
 		callback = function()
 			vim.cmd.redrawstatus()
 		end,
 	})
 end
-
 -- [[ Diagnostic Configuration ]]
 -- Define how errors and warnings are displayed globally.
 vim.diagnostic.config({
